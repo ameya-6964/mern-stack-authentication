635d36e04720bf8e172ddb8d9920aae023a832f6
ameya-6964 <belvalkar7@gmail.com>
2024-02-05 19:23:51 +0530

    Updated Server For Production Build

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import userRoutes from "./routes/userRoutes.js";
 import cookieParser from "cookie-parser";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
+import path from "path";
 dotenv.config();
 const PORT = process.env.PORT || 8000;
 
@@ -21,9 +22,18 @@ app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 
-app.get("/", (req, res) => {
-  res.send("<h1>Server Started</h1>");
-});
+if (process.env.NODE_ENV === "PRODUCTION") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
